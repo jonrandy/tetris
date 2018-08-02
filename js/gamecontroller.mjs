0c8f5632252cd805bacd25209f6ccbc99f9dfb90
Jon Randy <jonrandy@gmail.com>
2018-08-02 13:58:43 +0700

    Adds initial game controller stuff

---
+++ js/gamecontroller.mjs
@@ -0,0 +1,42 @@
+export function GameController(driver) {
+
+	let
+		_self
+	;
+
+	return _self = {
+		up,
+		down,
+		left,
+		right,
+		buttonA,
+		buttonB,
+		buttonSelect,
+		buttonStart
+	};
+
+}
+
+
+
+export function KeyboardDriver({
+
+} = {}) {
+
+	let
+		_self
+	;
+
+
+	return _self = {
+		up,
+		down,
+		left,
+		right,
+		buttonA,
+		buttonB,
+		buttonSelect,
+		buttonStart
+	};
+
+}
