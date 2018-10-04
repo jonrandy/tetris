a907f5e304551722d6eb6faf5d194b91520d6431
Jon Randy <jonrandy@gmail.com>
2018-10-04 17:44:50 +0700

    Tidies a bit

--- js/main.mjs
+++ js/main.mjs
@@ -13,7 +13,6 @@ const
 ;
 
 
-
 let GAME = ((controller, gameVisualiser)=>{
 
 	let
@@ -178,45 +177,5 @@ const GAMELOOP = () => {
 
 const gameId = window.setInterval(GAMELOOP, 50);
 
-
-
-// let myBoard = Board();
-// let createPiece = ()=>Piece({
-// 	x: Math.random()*10|0, //8|0,
-// 	y: myBoard.playHeight+3,
-// 	tile: Tetrominos[7] // Tetrominos[Math.random()*6|0]
-// }).rotate(1); //Math.random()*4|0);
-// let myPiece = createPiece();
-
-
-// let interval = window.setInterval(() => {
-// 	myPiece.moveRel(0,-1);
-// 	if (!myBoard.pieceFits(myPiece)) {
-// 		myPiece.undo();
-// 		myBoard.freeze([myPiece]);
-// 		let winners = myBoard.winningBlocks();
-// 		if (winners.length) {
-// 			myBoard.killBlocks(winners);
-// 		}
-// 		myPiece = createPiece();
-// 	}
-// 	draw(myBoard);
-
-// 	let s = "Up : " + controller.up + "\n";
-// 	s += "Down : " + controller.down + "\n";
-// 	s += "Left : " + controller.left + "\n";
-// 	s += "Right : " + controller.right + "\n";
-// 	s += "Button A : " + controller.buttonA + "\n";
-// 	s += "Button B : " + controller.buttonB + "\n";
-// 	s += "Select : " + controller.buttonSelect + "\n";
-// 	s += "Start : " + controller.buttonStart + "\n";
-// 	s += "Quit : " + controller.buttonQuit + "\n";
-// 	s += "Last On : " + controller.info.lastOn + "\n";
-// 	s += "Last Off : " + controller.info.lastOff + "\n";
-// 	s += "Buttons on : " + controller.info.buttonsOn + "\n";
-
-// 	k.value = s;
-
-// }, 5);
 
 
